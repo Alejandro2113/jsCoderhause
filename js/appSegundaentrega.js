39a661fa317adc49fbf41e6fc462a77728d5d085
Ing. Moises A. Gonzalez <gonza21132.1@gmail.com>
2024-05-22 16:24:59 -0600

    Tercer Entrega a un 90%

---
+++ js/appSegundaentrega.js
@@ -0,0 +1,398 @@
+/*Orden de pupusas (Me costo encontrat otro ejemplo) ver 2.3.0.
+Alumno= Moises Alejandro Gonzalez Moran.
+Comision CODERHAUSE javascrit (No "JAVA" sino me funean) #54005.
+Tutoría a cargo de Marcelo Furlong.
+*/
+
+/* DEJO CLAROS MIS OBJETIVOS PARA TRAZAR UNA META Y DEFINIR PUNTOS A TOCAR EN ESTA TERCER
+PREENTREGA
+
+*OBJETIVOS DE SEGUNDA ENTREGA
+
+//OBJETIVO PRIMARIO//
+- AGREGAR UN RECIBO EN DOM.
+- CREAR EVENTOS PARA QUITAR LOS PROMP.
+- MEJORAR LA BASE DE USUARIOS.
+- AGREGAR BOTONES PARA INGRESAR CLIENTE, O PARA BUSCAR CLIENTE Y SUS DATOS, (BUSCAR POR NUMERO DE TELEFONO Y NOMBRE).
+- ALMACENAR EN LOCAL STORAGE LA ORDEN Y EL CLINETE.
+-TEST 2
+*/
+
+alert(
+  "Programa para ordenar pupusas \n Aqui podes pedir por persona\n estas listo para iniciar?"
+);
+
+let personas = parseInt(prompt("Cuantas personas van a ordenar?"));
+
+//Funcion para depurando de valores !logicos
+function recarga() {
+  let entrada = confirm(
+    "Valores invalidos!!!\nTipos de error.\n\nTienes que ingresar cuantas personas (15 personas maximo).\nOrdenaste un numero negativo\nQuieres empezar de nuevo?"
+  );
+
+  if (entrada) {
+    location.reload();
+    throw new Error(
+      "Numero de personas Mayor al esperado, deteniendo el script"
+    );
+  } else {
+    alert("Un gusto atenderte, vuelve pronto");
+    throw new Error(
+      "Numero de personas Mayor al esperado, deteniendo el script"
+    );
+  }
+}
+
+//Depurado usando la funcion ya establecida
+if (personas <= 0 || isNaN(personas) || personas >= 15) {
+  recarga();
+} else if (personas >= 16) {
+  alert(
+    "Tu pedido es muy grande, porfavor contactanos al telefono para asegurar tu orden"
+  );
+}
+
+let pupusas = [0, 0, 0, 0, 0];
+
+//Aqui la funcion que te deja seleccionar y lo guarde
+function pupas() {
+  let revueltas = parseInt(
+    prompt("Cuantas revueltas quiere la perosna " + i + " ?.")
+  );
+  let frijol = parseInt(
+    prompt("Cuantas de frijol y queso quiere la perosna " + i + " ?.")
+  );
+  let campesina = parseInt(
+    prompt("Cuantas campesinas quiere la perosna " + i + " ?.")
+  );
+  let chicharon = parseInt(
+    prompt("Cuantas chicharon quiere la perosna " + i + " ?.")
+  );
+  let vegetariana = parseInt(
+    prompt("Cuantas vegetarianas quiere la perosna " + i + " ?.")
+  );
+
+  //Depurado usando la funcion ya establecida y limitando a 10 por tipo de pupusa.
+  if (revueltas < 0 || isNaN(revueltas) || revueltas >= 10) {
+    recarga();
+  }
+  if (frijol < 0 || isNaN(frijol) || frijol >= 10) {
+    recarga();
+  }
+  if (campesina < 0 || isNaN(campesina) || campesina >= 10) {
+    recarga();
+  }
+  if (chicharon < 0 || isNaN(chicharon) || chicharon >= 10) {
+    recarga();
+  }
+  if (vegetariana < 0 || isNaN(vegetariana) || vegetariana >= 10) {
+    recarga();
+  }
+
+  let pupas = [revueltas, frijol, campesina, chicharon, vegetariana];
+
+  //sumas use logica de sumas acumuladas (se que use objetos pero queria un orden en una base)
+  /*pupusas.revueltas = pupas.revueltas + pupusas.revueltas;
+  pupusas.frijol = pupas.frijol + pupusas.frijol;
+  pupusas.campesina = pupas.campesina + pupusas.campesina;
+  pupusas.chicharon = pupas.chicharon + pupusas.chicharon;
+  pupusas.vegetariana = pupas.vegetariana + pupusas.vegetariana;*/
+
+  //LO MODIFIQUE CON UN ARRAY ARRIBA ESTA EL CODIGO ANTERIOR.
+  for (n = 0; n < 5; n++) {
+    pupusas[n] = pupas[n] + pupusas[n];
+  }
+  console.log(pupas);
+  console.log(pupusas);
+
+  //return pupas;
+}
+
+alert(
+  "Actualmente el restaurante te ofrece 5 opciones de exquisitas pupusas.\n\n 1- Revueltas (Queso, frijoles, chicharon, hongos).\n 2- La queso (Queso y loroco).\n 3- Campesina (Queso, frijol).\n 4- La caperusita (Chicharron, queso).\n 5- La vegetariana (Espinaca, hongos, mora, ajo)."
+);
+
+//Ciclo para numero de personas (15 maximo establecido previamnete en linea 33)
+for (i = 1; i <= personas; i++) {
+  alert("¿Que pupusas quiere la persona " + i + "?.");
+  pupas();
+  //Pirmer valor para la suma
+}
+
+alert(
+  "El resumen de tu pedido es:\n" +
+    `${pupusas[0]} - Pupusas Revueltas\n` +
+    `${pupusas[1]} - Pupusas La queso\n` +
+    `${pupusas[2]} - Pupusas Campesina\n` +
+    `${pupusas[3]} - Pupusas La caperusita\n` +
+    `${pupusas[4]} - Pupusas La vegetariana\n`
+);
+
+//alert("Tu orden ya esta en la plancha, espero lo disfrutes =)");
+
+//-----------------------------------------------------------------------------------------//
+
+alert(
+  "Tu orden ya esta en cocina, Porfavor termina de llenar\n los datos para el envio."
+);
+
+class MiCliente {
+  constructor(idCliente, telefono, nombre, edad, direccion, alergias) {
+    this.idCliente = idCliente;
+    this.telefono = telefono;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.direccion = direccion;
+    this.alergias = alergias;
+  }
+}
+
+// Introcuciendo base de clientes
+
+const baseClientes = [
+  {
+    idCliente: 1,
+    nombre: "EDUARDO GONZALEZ",
+    telefono: 61616565,
+    edad: 21,
+    direccion: "avenida siempre viva 120",
+    alergias: "pimienta",
+  },
+  {
+    idCliente: 2,
+    nombre: "jULIO MEDINA",
+    telefono: 61616566,
+    edad: 22,
+    direccion: "avenida siempre viva 121",
+    alergias: "loroco",
+  },
+  {
+    idCliente: 3,
+    nombre: "JOSUE GONZALEZ",
+    telefono: 61616060,
+    edad: 23,
+    direccion: "avenida siempre viva 122",
+  },
+  {
+    idCliente: 4,
+    nombre: "HOMERO SIMPSON",
+    telefono: 70707878,
+    edad: 34,
+    direccion: "avenida siempre viva 123",
+    alergias: "pimienta",
+  },
+  {
+    idCliente: 5,
+    nombre: "GABRIELA GONZALEZ",
+    telefono: 61616000,
+    edad: 27,
+    direccion: "avenida siempre viva 124",
+  },
+  {
+    idCliente: 6,
+    nombre: "ALE",
+    telefono: 78707870,
+    edad: 30,
+    direccion: "avenida siempre viva 125",
+    alergias: "colorante rojo",
+  },
+];
+
+console.log("Base clientes= ", baseClientes);
+console.log("Numero de clientes= ", baseClientes.length);
+
+function crearCliente() {
+  let nombre = prompt("Escribe un nombre y un apellido");
+  let edad = parseInt(prompt("Que edad tienes?"));
+  let telefono = parseInt(
+    prompt("Escribe tu numero de telefono para contactarnos")
+  );
+  let direccion = prompt("Escribe tu direccion de envio");
+  let alergias = prompt("Eres alergico en alguna sustancia?");
+
+  const clienteNuevo = new MiCliente(
+    baseClientes.length + 1,
+    telefono,
+    nombre.toUpperCase(),
+    edad,
+    direccion.toLowerCase(),
+    alergias.toLowerCase()
+  );
+
+  console.log("cliente nuevo", clienteNuevo);
+  console.log("id de cliente ", clienteNuevo.idCliente);
+
+  let envio = confirm(
+    `Tu nombre es: ${clienteNuevo.nombre} \n` +
+      `Tu direccion es: ${clienteNuevo.direccion}\n` +
+      `Deseas que te enviemos a esta dirección?`
+  );
+
+  if (envio) {
+    //LO REGISTRO EN LA BASE DE DATOS
+    baseClientes.push(clienteNuevo);
+
+    clienteFinal = clienteNuevo.nombre;
+    direccionFinal = clienteNuevo.direccion;
+
+    console.log("Base clientes= ", baseClientes);
+    console.log("Numero de clientes= ", baseClientes.length);
+    alert("Tu orden esta en la plancha");
+  } else {
+    alert("Ingresa de nuevo tus datos de envio");
+    crearCliente();
+  }
+}
+
+function buscarClientes() {
+  let telefonoBuscado = parseInt(
+    prompt("Escribe tu numero de telefono para buscarte en la base de clientes")
+  );
+  console.log("Tu numero de telefono es: ", telefonoBuscado);
+  let buscarCliente = baseClientes.find(
+    (item) => item.telefono === telefonoBuscado
+  );
+
+  console.log(buscarCliente);
+
+  let clienteBuscado = prompt(
+    "Escribe el nombre que registraste"
+  ).toUpperCase();
+
+  if (buscarCliente != undefined && buscarCliente.nombre === clienteBuscado) {
+    console.log("Tu nombre es ", buscarCliente.nombre);
+    console.log("Tus datos son: ", buscarCliente);
+
+    clienteFinal = buscarCliente.nombre;
+    direccionFinal = buscarCliente.direccion;
+
+    let envio = confirm(
+      `Tu nombre es: ${buscarCliente.nombre} \n` +
+        `Tu direccion es: ${buscarCliente.direccion}\n` +
+        `Deseas que te enviemos a esta dirección?`
+    );
+    if (envio) {
+      alert("Tu orden esta en la plancha");
+    } else {
+      alert("Ingresa de nuevo tus datos de envio");
+      crearCliente();
+    }
+  } else {
+    let nuevabusqueda = confirm("Los datos no coinciden, deseas registrarte?");
+    if (nuevabusqueda) {
+      crearCliente();
+    } else {
+      alert(
+        "Necesitamos tus datos para el pedido, por favor ingresa de nuevo tus datos"
+      );
+      crearCliente;
+    }
+  }
+}
+
+let cliente = confirm(
+  "Eres cliente registrado? (pulsa aceptar)\n, o deseas registrarte? (pulsa cancelar)",
+  "Si ya lo estoy",
+  "NO LO ESTOY"
+);
+
+if (cliente) {
+  crearCliente();
+} else {
+  buscarClientes();
+}
+
+let TotalPupas = pupusas.reduce((a, b) => a + b, 0);
+let PrecioPupusas = [
+  pupusas[0] * 1.0,
+  pupusas[1] * 1.25,
+  pupusas[2] * 1.15,
+  pupusas[3] * 1.2,
+  pupusas[4] * 0.85,
+];
+let TotalPrecio = PrecioPupusas.reduce((a, b) => a + b, 0);
+
+//DOM---- PARA MOSTRAR EL PEDIDO Y LOS DATOS DE ENVIO.
+//id="pedidos"
+
+let divReibos = document.createElement("div");
+divReibos.setAttribute("id", "pedidos");
+divReibos.setAttribute("class", "pedidos");
+
+const recibofinal = document.getElementById("pedidos");
+const nombreRecibo = document.createElement("h3");
+nombreRecibo.innerText = "DETALLES DE TU ORDEN";
+const horaPedido = document.createElement("p");
+horaPedido.innerText = `Hora del pedido:\n${Date()}`;
+const nombreCliente = document.createElement("p");
+nombreCliente.innerText = `Orde a nombre de: ${clienteFinal}`;
+const direccionRecibo = document.createElement("p");
+direccionRecibo.innerText = `Tu direccion es: ${direccionFinal}`;
+const resumenPupusas = document.createElement("p");
+resumenPupusas.innerText =
+  "El resumen de tu pedido es:\n\n" +
+  `${pupusas[0]} - Pupusas Revueltas\t - (1.00)\t - $${(
+    pupusas[0] * 1.0
+  ).toFixed(2)}\n` +
+  `${pupusas[1]} - Pupusas La queso\t - (1.25)\t - $${(
+    pupusas[1] * 1.25
+  ).toFixed(2)}\n` +
+  `${pupusas[2]} - Pupusas Campesina\t - (1.15)\t - $${(
+    pupusas[2] * 1.15
+  ).toFixed(2)}\n` +
+  `${pupusas[3]} - Pupusas La caperusita\t - (1.20)\t - $${(
+    pupusas[3] * 1.2
+  ).toFixed(2)}\n` +
+  `${pupusas[4]} - Pupusas La vegetariana\t - (0.85)\t - $${(
+    pupusas[4] * 0.85
+  ).toFixed(2)}\n 
+    En total son ${TotalPupas} Pupusas
+    Cantidad a pagar = $ ${TotalPrecio.toFixed(2)}`;
+
+
+recibofinal.appendChild(nombreRecibo);
+recibofinal.appendChild(horaPedido);
+recibofinal.appendChild(nombreCliente);
+recibofinal.appendChild(direccionRecibo);
+recibofinal.appendChild(resumenPupusas);
+divReibos.appendChild(recibofinal);
+
+//codigo a revisar
+
+
+const recibofinal = document.getElementById("Datos");
+const nombreRecibo = document.createElement("h3");
+nombreRecibo.innerText = "DETALLES DE TU ORDEN";
+const horaPedido = document.createElement("p");
+horaPedido.innerText = `Hora del pedido:\n${Date()}`;
+const nombreCliente = document.createElement("p");
+nombreCliente.innerText = `Orde a nombre de: {clienteFinal}`;
+const direccionRecibo = document.createElement("p");
+direccionRecibo.innerText = `Tu direccion es: {direccionFinal}`;
+const resumenPupusas = document.createElement("p");
+resumenPupusas.innerText =
+  "El resumen de tu pedido es:\n\n" +
+  `${pupas[0]} - Pupusas Revueltas\t - (1.00)\t - $${(
+    pupas[0] * 1.0
+  ).toFixed(2)}\n` +
+  `${pupas[1]} - Pupusas La queso\t - (1.25)\t - $${(
+    pupas[1] * 1.25
+  ).toFixed(2)}\n` +
+  `${pupas[2]} - Pupusas Campesina\t - (1.15)\t - $${(
+    pupas[2] * 1.15
+  ).toFixed(2)}\n` +
+  `${pupas[3]} - Pupusas La caperusita\t - (1.20)\t - $${(
+    pupas[3] * 1.2
+  ).toFixed(2)}\n` +
+  `${pupas[4]} - Pupusas La vegetariana\t - (0.85)\t - $${(
+    pupas[4] * 0.85
+  ).toFixed(2)}\n 
+En total son ${TotalPupas} Pupusas
+Cantidad a pagar = $ ${TotalPrecio.toFixed(2)}`;
+
+recibofinal.appendChild(nombreRecibo);
+recibofinal.appendChild(horaPedido);
+recibofinal.appendChild(nombreCliente);
+recibofinal.appendChild(direccionRecibo);
+recibofinal.appendChild(resumenPupusas);
+divReibos.appendChild(recibofinal);
--- js/appTercerEntrega.js
+++ js/appTercerEntrega.js
@@ -24,55 +24,81 @@ ESFUERZO (5MIN) Y QUIERO QUE QUEDE A POSTERIDAD.
 
 //BASE DE CLIENTES
 
-const baseClientes = [
-  {
-    idCliente: 1,
-    nombre: "EDUARDO GONZALEZ",
-    telefono: 61616565,
-    edad: 21,
-    direccion: "avenida siempre viva 120",
-    alergias: "pimienta",
-  },
-  {
-    idCliente: 2,
-    nombre: "jULIO MEDINA",
-    telefono: 61616566,
-    edad: 22,
-    direccion: "avenida siempre viva 121",
-    alergias: "loroco",
-  },
-  {
-    idCliente: 3,
-    nombre: "JOSUE GONZALEZ",
-    telefono: 61616060,
-    edad: 23,
-    direccion: "avenida siempre viva 122",
-  },
-  {
-    idCliente: 4,
-    nombre: "HOMERO SIMPSON",
-    telefono: 70707878,
-    edad: 34,
-    direccion: "avenida siempre viva 123",
-    alergias: "pimienta",
-  },
-  {
-    idCliente: 5,
-    nombre: "GABRIELA GONZALEZ",
-    telefono: 61616000,
-    edad: 27,
-    direccion: "avenida siempre viva 124",
-  },
-  {
-    idCliente: 6,
-    nombre: "ALE",
-    telefono: 78707870,
-    edad: 30,
-    direccion: "avenida siempre viva 125",
-    alergias: "colorante rojo",
-  },
-];
+class baseCliente {
+  constructor(idCliente, telefono, nombre, edad, direccion, alergias) {
+    this.idCliente = idCliente;
+    this.telefono = telefono;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.direccion = direccion;
+    this.alergias = alergias;
+  }
+}
 
+console.log(localStorage.length);
+
+if (localStorage.length < 4) {
+  let baseCliente = [
+    {
+      idCliente: 1,
+      nombre: "EDUARDO GONZALEZ",
+      telefono: 61616565,
+      edad: 21,
+      direccion: "avenida siempre viva 120",
+      alergias: "pimienta",
+    },
+    {
+      idCliente: 2,
+      nombre: "jULIO MEDINA",
+      telefono: 61616566,
+      edad: 22,
+      direccion: "avenida siempre viva 121",
+      alergias: "loroco",
+      alergias: "chile",
+    },
+    {
+      idCliente: 3,
+      nombre: "JOSUE GONZALEZ",
+      telefono: 61616060,
+      edad: 23,
+      direccion: "avenida siempre viva 122",
+      alergias: "colorantes",
+    },
+    {
+      idCliente: 4,
+      nombre: "HOMERO SIMPSON",
+      telefono: 70707878,
+      edad: 34,
+      direccion: "avenida siempre viva 123",
+      alergias: "pimienta",
+    },
+    {
+      idCliente: 5,
+      nombre: "GABRIELA GONZALEZ",
+      telefono: 61616000,
+      edad: 27,
+      direccion: "avenida siempre viva 124",
+      alergias: "chicharon",
+    },
+    {
+      idCliente: 6,
+      nombre: "ALE",
+      telefono: 78707870,
+      edad: 30,
+      direccion: "avenida siempre viva 125",
+      alergias: "colorante rojo",
+    },
+  ];
+  console.log(baseCliente);
+
+  baseClientes = baseCliente;
+} else if (localStorage.length >= 4) {
+  let baseCliente = JSON.parse(localStorage.getItem("baseClientes"));
+  console.log(baseCliente);
+  baseClientes = baseCliente;
+}
+
+console.log(baseClientes);
 class MiCliente {
   constructor(idCliente, telefono, nombre, edad, direccion, alergias) {
     this.idCliente = idCliente;
@@ -84,28 +110,10 @@ class MiCliente {
   }
 }
 
-//Funcion para depurando de valores !logicos
-function recarga() {
-  let entrada = confirm(
-    "Valores invalidos!!!\nTipos de error.\n\nTienes que ingresar cuantas personas (15 personas maximo).\nOrdenaste un numero negativo\nQuieres empezar de nuevo?"
-  );
-
-  if (entrada) {
-    location.reload();
-    throw new Error(
-      "Numero de personas Mayor al esperado, deteniendo el script"
-    );
-  } else {
-    alert("Un gusto atenderte, vuelve pronto");
-    throw new Error(
-      "Numero de personas Mayor al esperado, deteniendo el script"
-    );
-  }
-}
-
 let pupas = [{}, {}, {}, {}, {}];
+
 //FUNCION QUE REVISA SI EXISTE UN PEDIDO ANTERIOR
-if (localStorage.length >= 5) {
+if (localStorage.length >= 2) {
   const pedidoInicial = document.getElementById("datosPedido");
   let revueltas = document.getElementById("rev");
   let queso = document.getElementById("fri");
@@ -113,11 +121,13 @@ if (localStorage.length >= 5) {
   let chicharon = document.getElementById("chich");
   let vegetariana = document.getElementById("vege");
 
-  revueltas.value = Number(localStorage.getItem(`revueltas`));
-  queso.value = Number(localStorage.getItem(`queso`));
-  campesina.value = Number(localStorage.getItem(`campesina`));
-  chicharon.value = Number(localStorage.getItem(`chicharon`));
-  vegetariana.value = Number(localStorage.getItem(`vegetariana`));
+  let pupas = JSON.parse(localStorage.getItem("pupas"));
+  console.log(pupas);
+  revueltas.value = pupas[0];
+  queso.value = pupas[1];
+  campesina.value = pupas[2];
+  chicharon.value = pupas[3];
+  vegetariana.value = pupas[4];
 }
 
 // INGRESANDO VALORES CON EVENTOS.
@@ -164,11 +174,7 @@ botonPupusas.onclick = () => {
   let TotalPrecio = PrecioPupas.reduce((a, b) => a + b, 0);
 
   //ALMACEN DE DATOS PARA NO PERDER EL PEDIDO
-  localStorage.setItem("revueltas", pupas[0]);
-  localStorage.setItem("queso", pupas[1]);
-  localStorage.setItem("campesina", pupas[2]);
-  localStorage.setItem("chicharon", pupas[3]);
-  localStorage.setItem("vegetariana", pupas[4]);
+  localStorage.setItem("pupas", JSON.stringify(pupas));
   localStorage.setItem("TotalPupas", TotalPupas);
   localStorage.setItem("TotalPrecio", TotalPrecio.toFixed(2));
 
@@ -176,50 +182,79 @@ botonPupusas.onclick = () => {
   console.log("TOTAL PUPUSAS = " + TotalPupas);
   console.log("$ " + TotalPrecio.toFixed(2));
 
-  if (
-    pupas[0] == 0 &&
-    pupas[1] == 0 &&
-    pupas[2] == 0 &&
-    pupas[3] == 0 &&
-    pupas[4] == 0
-  ) {
-    //DEJARE SOLO ESTE ALERT PARA PODER ADVERTIR UNA VEZ QUE NO SE INGRESARON DATOS
-    localStorage.clear();
-    alert(
-      "Porfavor, ingresa cuantas pupas y luego pulsa en enviar para poder tomar tu orden"
-    );
-    location.reload();
-  }
-
   let clienteFinal = document.getElementById("nombre");
   let edadCliente = document.getElementById("age");
   let telefonoCleinte = document.getElementById("telefono");
   let direccionCliente = document.getElementById("direccion");
   let alergiaCliente = document.getElementById("alergia");
 
-  console.log("Cliente= "+clienteFinal.value);
-  console.log("Edad= "+edadCliente.value);
-  console.log("Telefono= "+telefonoCleinte.value);
-  console.log("Direccion= "+direccionCliente.value);
-  console.log("Alergias= "+alergiaCliente.value);
+  console.log("Cliente= " + clienteFinal.value);
+  console.log("Edad= " + edadCliente.value);
+  console.log("Telefono= " + telefonoCleinte.value);
+  console.log("Direccion= " + direccionCliente.value);
+  console.log("Alergias= " + alergiaCliente.value);
 
-  clienteNuevo = new MiCliente (baseClientes.length+1,telefonoCleinte.value,clienteFinal.value,edadCliente.value,direccionCliente.value,alergiaCliente.value);
+  clienteNuevo = new MiCliente(
+    baseClientes.length + 1,
+    telefonoCleinte.value,
+    clienteFinal.value,
+    edadCliente.value,
+    direccionCliente.value,
+    alergiaCliente.value
+  );
 
+  if (clienteFinal.value == 0) {
+    alert(
+      "Porfavor, ingresa cuantas pupas y luego tu nombre completo y direccion antes de pulsar el boton\n para enviar tu orden"
+    );
+    location.reload();
+  }
+
+  if (direccionCliente.value == 0) {
+    direccionCliente.value = "Recojer en tienda";
+  }
+
+  if (
+    pupas[0] == 0 &&
+    pupas[1] == 0 &&
+    pupas[2] == 0 &&
+    pupas[3] == 0 &&
+    pupas[4] == 0 &&
+    direccionCliente.value == 0
+  ) {
+    //DEJARE SOLO ESTE ALERT PARA PODER ADVERTIR UNA VEZ QUE NO SE INGRESARON DATOS
+    localStorage.clear();
+    alert(
+      "Porfavor, ingresa cuantas pupas y luego tu direccion antes de pulsar el boton\n para enviar tu orden"
+    );
+    location.reload();
+  }
   botonPupusas.remove();
 
-  if (localStorage.length >= 7) {
+  if (localStorage.length >= 3) {
     const divCliente = document.getElementById("cliente");
     const divContenedor = document.getElementById("datosPedido");
 
     let botonAlmacenarCliente = document.createElement("button");
-    botonAlmacenarCliente.setAttribute("class", "botonDireccion");
-    botonAlmacenarCliente.setAttribute("id", "botonDireccion");
-    botonAlmacenarCliente.textContent = "Almacenar mis datos";
+
+    let buscartelefono = baseClientes.find(
+      (item) => item.telefono === telefonoCleinte.value
+    );
+
+    if (
+      buscartelefono === undefined &&
+      direccionCliente.value != "Recojer en tienda"
+    ) {
+      botonAlmacenarCliente.setAttribute("class", "botonDireccion");
+      botonAlmacenarCliente.setAttribute("id", "botonDireccion");
+      botonAlmacenarCliente.textContent = "Almacenar mis datos";
+    }
     botonAlmacenarCliente.onclick = () => {
       //PUSHEAR LOS DAROS A LA BASE DE DATOS DEL CLIENTES Y ALMACENAR
       baseClientes.push(clienteNuevo);
-
+      localStorage.setItem("baseClientes", JSON.stringify(baseClientes));
       botonAlmacenarCliente.remove();
+      console.log(baseClientes);
     };
     divCliente.appendChild(botonAlmacenarCliente);
 
@@ -252,11 +287,11 @@ botonPupusas.onclick = () => {
       const resumenPupusas = document.createElement("p");
       resumenPupusas.innerText =
         "El resumen de tu pedido es:\n\n" +
-        `${pupas[0]} - Pupusas Revueltas\t - (1.00)\t - $ ${(
-          pupas[0] * 1.0
+        `${pupas[0]} - Pupusas Revueltas\t - (0.90)\t - $ ${(
+          pupas[0] * 0.9
         ).toFixed(2)}\n` +
-        `${pupas[1]} - Pupusas La queso\t - (1.25)\t - $ ${(
-          pupas[1] * 1.25
+        `${pupas[1]} - Pupusas La queso\t - (0.95)\t - $ ${(
+          pupas[1] * 0.95
         ).toFixed(2)}\n` +
         `${pupas[2]} - Pupusas Campesina\t - (1.15)\t - $ ${(
           pupas[2] * 1.15
